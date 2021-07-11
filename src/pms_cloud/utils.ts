61fe3dccd372388b055986b4f6dde1cad6c0ec04
quipex <rudas72@gmail.com>
2021-07-11 22:03:57 +0300

    Added sleep function

--- src/pms_cloud/utils.ts
+++ src/pms_cloud/utils.ts
@@ -1,3 +1,9 @@
+import util from 'util';
+
 export function urlEncode(object: any): string {
 	return encodeURIComponent(JSON.stringify(object));
+}
+
+export async function sleep(ms: number): Promise<any> {
+	return util.promisify(setTimeout)(ms);
 }
